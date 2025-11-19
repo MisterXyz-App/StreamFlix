f7997a15912cae9a0ef82170b6a718fbb0eb4e9f
MisterXyz-App <vanjr890@gmail.com>
2025-11-20 05:03:26 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'streamflix-secret-key-2024'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Cloudinary Configuration - TAMBAHKAN INI
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dzfkklsza')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY', '588474134734416')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET', '9c12YJe5rZSYSg7zROQuvmVZ7mg')
+
+    # --- Railway Compatibility (TANPA MENGUBAH FUNGSI LAMA) ---
+    # Gunakan postgres:// → menjadi postgresql://
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
